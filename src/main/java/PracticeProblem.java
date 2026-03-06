b4fc3de9fef483c786d7cf61c21d33a95f146c19
ArthurYang80 <342802865@tdsb.ca>
2026-03-06 03:03:17 +0000

    Finalized

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,74 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+	
 	}
 
+	public static String readFile(String filename) {
+	FileReader inputStream = null;
+	String content = "";
 	
+		try {
+			inputStream = new FileReader(filename);
+			int c;
+		
+				while ((c = inputStream.read()) != -1) {
+					content += (char)c;
+				}
+		
+			return content;
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		return "";
+		}
+		finally {
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			}
+			catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+	}
 
+	public static String backwardsReadFile(String filename) {
+	FileReader inputStream = null;
+	String content = "";
+	String reversed = "";
+	
+		try {
+			inputStream = new FileReader(filename);
+			int c;
+		
+			while ((c = inputStream.read()) != -1) {
+				content += (char)c;
+			}
+		
+			for (int i = content.length() - 1; i >= 0; i--) {
+				reversed += content.charAt(i);
+			}
+		
+			return reversed;
+		}
+			catch (IOException e) {
+				System.out.println(e);
+			return "";
+		}
+		finally {
+			try {
+			if (inputStream != null) {
+				inputStream.close();
+				}
+			}
+			catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+	}
 }
